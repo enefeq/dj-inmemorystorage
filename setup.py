37023fd7d4b01251b8594410e28c809ca2da5c57
Wes Okes <wes.okes@gmail.com>
2017-11-09 12:27:46 -0500

    python and django

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from setuptools import setup
 
-requires = ['Django >= 1.4', 'six>=1.4.1']
+requires = ['Django>=1.9', 'six>=1.4.1']
 tests_require = requires
 
 setup(
     name="ambition-inmemorystorage",
     description="A non-persistent in-memory data storage backend for Django.",
-    version="1.4.1",
+    version="1.5.0",
     url="https://github.com/waveaccounting/dj-inmemorystorage",
     license=open('LICENSE').read(),
     long_description=open('README.rst').read(),
@@ -29,12 +29,9 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
